824b9d7d9d617841785cf894ce3dfa6584c68547
Apple-Kong <kongthn@gmail.com>
2022-01-08 19:43:13 +0900

    initial set up and pod installed

---
+++ BGJTCloneApp/Configuration/Base/BaseViewController.swift
@@ -0,0 +1,25 @@
+//
+//  BaseViewController.swift
+//  EduTemplate - storyboard
+//
+//  Created by Zero Yoon on 2022/02/23.
+//
+
+import UIKit
+
+
+//모든 뷰 컨트롤러 마다 공통적으로 들어가는 사항을 포함시키면 된다.
+class BaseViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // MARK: 아래 예시들처럼 상황에 맞게 활용하시면 됩니다. 네비게이션 바 글꼴이나 이런것들.
+        
+        // Navigation Bar
+//        self.navigationController?.navigationBar.titleTextAttributes = [
+//            .font : UIFont.NotoSans(.medium, size: 16),
+//        ]
+        // Background Color
+//        self.view.backgroundColor = .white
+    }
+}
---
+++ BGJTCloneApp/Configuration/Constant.swift
@@ -0,0 +1,12 @@
+//
+//  Constant.swift
+//  EduTemplate - storyboard
+//
+//  Created by Zero Yoon on 2022/02/23.
+//
+
+
+
+struct Constant {
+    
+}
