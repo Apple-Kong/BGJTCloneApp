224e4987fc98c7ae007c4356826e464115fd5380
Apple-Kong <kongthn@gmail.com>
2022-01-10 01:03:48 +0900

    addView image collection View implemented

---
+++ BGJTCloneApp/Source/Login/Add/ImageCollectionReusableView.swift
@@ -0,0 +1,12 @@
+//
+//  ImageCollectionReusableView.swift
+//  BGJTCloneApp
+//
+//  Created by GOngTAE on 2022/01/10.
+//
+
+import UIKit
+
+class ImageCollectionReusableView: UICollectionReusableView {
+        
+}
