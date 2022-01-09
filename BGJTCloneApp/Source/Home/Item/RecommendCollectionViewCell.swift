fb97265c0e62aa57653576e112f2a775113da930
Apple-Kong <kongthn@gmail.com>
2022-01-09 18:17:06 +0900

    main item collection view

---
+++ BGJTCloneApp/Source/Home/Item/RecommendCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  RecommendCollectionViewCell.swift
+//  BGJTCloneApp
+//
+//  Created by GOngTAE on 2022/01/09.
+//
+
+import UIKit
+
+class RecommendCollectionViewCell: UICollectionViewCell {
+    
+}
