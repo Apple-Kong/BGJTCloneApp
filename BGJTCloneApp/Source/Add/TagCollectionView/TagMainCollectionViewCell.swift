04cbe5c2ca07137bdfea1c7975377ddbea9868bc
Apple-Kong <kongthn@gmail.com>
2022-01-13 00:12:23 +0900

    tag View detail updated, tag data communication success

---
+++ BGJTCloneApp/Source/Add/TagCollectionView/TagMainCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  TagMainCollectionViewCell.swift
+//  BGJTCloneApp
+//
+//  Created by GOngTAE on 2022/01/12.
+//
+
+import UIKit
+
+class TagMainCollectionViewCell: UICollectionViewCell {
+    
+}
