081fd56a73ce507d4e7c385850ac053a35478027
Apple-Kong <kongthn@gmail.com>
2022-01-12 12:32:56 +0900

    category table View implemented

---
+++ BGJTCloneApp/Source/Add/Category/Cell/AllTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  AllTableViewCell.swift
+//  BGJTCloneApp
+//
+//  Created by GOngTAE on 2022/01/12.
+//
+
+import UIKit
+
+class AllTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ BGJTCloneApp/Source/Add/Category/Cell/RecentTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  RecentTableViewCell.swift
+//  BGJTCloneApp
+//
+//  Created by GOngTAE on 2022/01/12.
+//
+
+import UIKit
+
+class RecentTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
