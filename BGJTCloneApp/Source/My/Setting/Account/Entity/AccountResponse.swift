aadeb6b58c8bbe44ce45a053fb828d1b77760b56
Apple-Kong <kongthn@gmail.com>
2022-01-16 21:01:43 +0900

    account get, add, delete success

---
+++ BGJTCloneApp/Source/My/Setting/Account/Entity/AccountResponse.swift
@@ -0,0 +1,8 @@
+//
+//  AccountResponse.swift
+//  BGJTCloneApp
+//
+//  Created by GOngTAE on 2022/01/16.
+//
+
+import Foundation
---
+++ BGJTCloneApp/Source/My/Setting/Account/MoreEdit/MoreModalViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MoreModalViewController.swift
+//  BGJTCloneApp
+//
+//  Created by GOngTAE on 2022/01/16.
+//
+
+import UIKit
+
+class MoreModalViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func editButtonTap(_ sender: UIButton) {
+    }
+}
